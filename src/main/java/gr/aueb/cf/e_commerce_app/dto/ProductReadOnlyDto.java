0d6f99166a53a4af76af7cc5b5e815f515ebc787
LiakosV <billiak@hotmail.com>
2025-07-01 19:05:13 +0300

    feat: ProductReadOnlyDto created

---
+++ src/main/java/gr/aueb/cf/e_commerce_app/dto/ProductReadOnlyDto.java
@@ -0,0 +1,20 @@
+package gr.aueb.cf.e_commerce_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductReadOnlyDto {
+
+    private Long id;
+
+    private String uuid;
+    private String name;
+    private String description;
+    private Boolean isActive;
+    private Integer quantity;
+    private Double price;
+}
