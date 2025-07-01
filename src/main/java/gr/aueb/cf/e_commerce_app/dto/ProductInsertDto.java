7f7c2f93e3166545ab0499c487eaa5edd96db6c1
LiakosV <billiak@hotmail.com>
2025-07-01 19:05:00 +0300

    feat: ProductInsertDto created

---
+++ src/main/java/gr/aueb/cf/e_commerce_app/dto/ProductInsertDto.java
@@ -0,0 +1,25 @@
+package gr.aueb.cf.e_commerce_app.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductInsertDto {
+
+    @NotEmpty(message = "The name must not be empty")
+    private String name;
+
+    @NotNull(message = "The price must not be null")
+    private Double price;
+
+    @NotEmpty(message = "The description must not be empty")
+    private String description;
+
+    @NotNull(message = "The quantity must not be null")
+    private Integer quantity;
+}
