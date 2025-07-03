ff66f88410b66ce93f325c10731f01ab435584f2
LiakosV <billiak@hotmail.com>
2025-07-03 12:11:29 +0300

    feat: a categories.sql script created

---
+++ src/main/resources/sql/categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO categories (id, name) VALUES
+                            (1, 'Laptops'),
+                            (2, 'Phones'),
+                            (3, 'Tablets'),
+                            (4, 'Smartwatches'),
+                            (5, 'Headphones'),
+                            (6, 'Accessories'),
+                            (7, 'Gaming'),
+                            (8, 'Networking'),
+                            (9, 'Storage'),
+                            (10, 'Drones');
+ALTER TABLE categories AUTO_INCREMENT = 11;
