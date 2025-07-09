6bf0f59da2fa14a4fd9fa48da0ca0f8f9e7c6159
LiakosV <billiak@hotmail.com>
2025-07-09 10:59:08 +0300

    feat: order dto created

---
+++ src/main/java/gr/aueb/cf/e_commerce_app/dto/OrderInsertDto.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.e_commerce_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderInsertDto {
+
+//    private Long userId;
+    private List<OrderItemInsertDto> items;
+}
---
+++ src/main/java/gr/aueb/cf/e_commerce_app/dto/OrderReadOnlyDto.java
@@ -0,0 +1,19 @@
+package gr.aueb.cf.e_commerce_app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderReadOnlyDto {
+
+    private Long orderId;
+    private Long userId;
+    private List<OrderItemReadOnlyDto> items;
+    private BigDecimal totalAmount;
+}
