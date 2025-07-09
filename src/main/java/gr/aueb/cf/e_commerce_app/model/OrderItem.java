f2e4ae9841dacd680296b5dd784080eb0cdd3384
LiakosV <billiak@hotmail.com>
2025-07-09 10:58:41 +0300

    feat: order item entity created

---
+++ src/main/java/gr/aueb/cf/e_commerce_app/model/OrderItem.java
@@ -0,0 +1,38 @@
+package gr.aueb.cf.e_commerce_app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    private BigDecimal unitPrice;
+
+    public BigDecimal getTotalPrice() {
+        return unitPrice.multiply(BigDecimal.valueOf(quantity));
+    }
+
+}
