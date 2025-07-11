fb7b6302ad06fbac7e22cf0a2ba735f5fcd57e82
LiakosV <billiak@hotmail.com>
2025-07-11 21:39:16 +0300

    fix: addressNumber changed from Integer to String and region changed to String regionName

--- src/main/java/gr/aueb/cf/e_commerce_app/dto/UserMoreInfoReadOnlyDto.java
+++ src/main/java/gr/aueb/cf/e_commerce_app/dto/UserMoreInfoReadOnlyDto.java
@@ -1,7 +1,6 @@
 package gr.aueb.cf.e_commerce_app.dto;
 
 import gr.aueb.cf.e_commerce_app.core.enums.Gender;
-import gr.aueb.cf.e_commerce_app.model.static_data.Region;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +12,8 @@ public class UserMoreInfoReadOnlyDto {
 
     private String phoneNumber;
     private Gender gender;
-    private Region region;
+    private String regionName;
     private String address;
-    private Integer addressNumber;
+    private String addressNumber;
     private String profilePhotoUrl;
 }
