071064f6c6b6cc7a9015a8440edf19225c61b6e4
LiakosV <billiak@hotmail.com>
2025-07-11 21:16:30 +0300

    feat: Operation for swagger added

--- src/main/java/gr/aueb/cf/e_commerce_app/rest/AuthRestController.java
+++ src/main/java/gr/aueb/cf/e_commerce_app/rest/AuthRestController.java
@@ -2,13 +2,20 @@ package gr.aueb.cf.e_commerce_app.rest;
 
 import gr.aueb.cf.e_commerce_app.authentication.AuthenticationService;
 import gr.aueb.cf.e_commerce_app.core.exceptions.AppObjectNotAuthorizedException;
+import gr.aueb.cf.e_commerce_app.core.exceptions.ValidationException;
 import gr.aueb.cf.e_commerce_app.dto.AuthenticationRequestDto;
 import gr.aueb.cf.e_commerce_app.dto.AuthenticationResponseDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,9 +29,24 @@ public class AuthRestController {
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthRestController.class);
     private final AuthenticationService authenticationService;
 
+    @Operation(
+            summary = "Authenticate a user",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User authenticated",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponseDto.class)
+                            )
+                    ),
+                    @ApiResponse(responseCode = "401", description = "Not authorized", content = @Content)
+            }
+    )
     @PostMapping("authenticate")
     public ResponseEntity<AuthenticationResponseDto> authenticate(@RequestBody AuthenticationRequestDto authenticationRequestDto)
-        throws AppObjectNotAuthorizedException {
+            throws AppObjectNotAuthorizedException {
+
 
         AuthenticationResponseDto authenticationResponseDto = authenticationService.authenticate(authenticationRequestDto);
         LOGGER.info("User authenticated");
