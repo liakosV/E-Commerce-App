ff8e5fc1262db88f2c098a20dce282311b0f76d2
LiakosV <billiak@hotmail.com>
2025-07-11 21:28:15 +0300

    fix: findByUuid added

--- src/main/java/gr/aueb/cf/e_commerce_app/repository/OrderRepository.java
+++ src/main/java/gr/aueb/cf/e_commerce_app/repository/OrderRepository.java
@@ -3,5 +3,9 @@ package gr.aueb.cf.e_commerce_app.repository;
 import gr.aueb.cf.e_commerce_app.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Optional<Order> findByUuid(String uuid);
 }
