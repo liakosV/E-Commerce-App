583f3cf409477a0a6dab67fefd6bb6f0c6f5e07c
LiakosV <billiak@hotmail.com>
2025-07-16 20:01:45 +0300

    feat: new routes added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 export const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {
+  path: 'categories/:id/products',
+  loadComponent: () =>
+    import('./components/category-products/category-products.component').then(
+      (m) => m.CategoryProductsComponent
+    ),
+}
 ];
