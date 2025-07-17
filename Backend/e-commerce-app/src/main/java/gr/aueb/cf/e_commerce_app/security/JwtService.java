559d55715d0a4c874d574f91afe5d4cd22820a4e
LiakosV <billiak@hotmail.com>
2025-07-17 19:00:03 +0300

    fix: changed the generateToken to take Map<> instead of string

--- Backend/e-commerce-app/src/main/java/gr/aueb/cf/e_commerce_app/security/JwtService.java
+++ Backend/e-commerce-app/src/main/java/gr/aueb/cf/e_commerce_app/security/JwtService.java
@@ -11,23 +11,16 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
-    //    private String secretKey = System.getenv("SECRET_KEY");
-//    private String secretKey = "FvArDZiJ1hvR9k3Ks1J6s8FqbmL6rRnlmTL5J3jNiT8";
 
-    //    Strong security 384-bits = 48 bytes = 64 Base64URL characters
     private String secretKey = "5ce98d378ec88ea09ba8bcd511ef23645f04cc8e70b9134b98723a53c275bbc5";
     private long jwtExpiration = 10800000;  // 3 hours in milliseconds
 
-//    if use refresh expiration token
-//    private long refreshExpiration = 604800000;
-
-    public String generateToken(String username, String role) {
-        var claims = new HashMap<String, Object>();
-        claims.put("role", role);
+    public String generateToken(String username, Map<String, Object> claims) {
         return Jwts
                 .builder()
                 .setIssuer("self")
