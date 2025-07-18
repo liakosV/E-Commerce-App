e200fb23d6ced1ee9533827275414430cd572e9b
LiakosV <billiak@hotmail.com>
2025-07-18 20:38:51 +0300

    fix more imports added and add to cart method created

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,31 +1,44 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Product } from '../../shared/interfaces/product';
 import { ProductService } from '../../shared/services/product.service';
-
+import { AuthService } from '../../shared/services/auth.service';
+import { CartService } from '../../shared/services/cart.service';
 import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSort, Sort } from '@angular/material/sort';
+import { RouterModule } from '@angular/router';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
 @Component({
   selector: 'app-home',
     imports: [
     CommonModule,
+    RouterModule,
+    FormsModule,
     MatTableModule,
     MatSortModule,
     MatIconModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   @ViewChild(MatSort) sort!: MatSort;
+  authService = inject(AuthService);
+  cartService = inject(CartService)
+  snackbar = inject(MatSnackBar);
 
   displayedColumns: string[] = ['name', 'price', 'description', 'quantity', 'isActive'];
+  popupOpen: { [productUuid: string]: boolean } = {};
+  selectedQuantities: { [productUuid: string]: number } = {};
   products: Product[] = [];
   totalPages = 0;
   page = 0;
@@ -36,6 +49,51 @@ export class HomeComponent {
 
   constructor(private productService: ProductService) {
     this.loadProducts();
+  }
+
+  ngOnInit(): void {
+    this.displayedColumns.push('actions');
+  }
+
+  get isAdminOrSeller(): boolean {
+    const role = this.authService.getUserRole();
+    return role === 'ADMIN' || role === 'SELLER'
+  }
+
+togglePopup(productUuid: string): void {
+  this.popupOpen[productUuid] = !this.popupOpen[productUuid];
+  this.selectedQuantities[productUuid] = 1;
+}
+
+addToCart(product: Product): void {
+  const selectedQuantity = this.selectedQuantities[product.uuid];
+
+  if(!selectedQuantity || selectedQuantity < 1 || selectedQuantity % 1 !== 0) {
+    this.snackbar.open('Invalid quantity selected', 'Close', {duration: 3000});
+    return;
+  }
+
+  if (product.quantity === 0) {
+    this.snackbar.open('Product is out of stock', 'Close', { duration: 3000 });
+    return;
+  }
+
+  if (selectedQuantity > product.quantity) {
+    this.snackbar.open('Quantity exceeds available stock', 'Close', { duration: 3000 });
+    return;
+  }
+
+  this.cartService.addItem({
+    productUuid: product.uuid,
+    name: product.name,
+    price: product.price,
+    quantity: selectedQuantity,
+  });
+
+  this.snackbar.open('Added to cart', 'Close', { duration: 2000 });
+
+  // Close popup after adding
+  this.popupOpen[product.uuid] = false;
   }
 
   loadProducts(): void {
