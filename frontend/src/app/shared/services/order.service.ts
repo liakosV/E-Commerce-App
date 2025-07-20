c7abd905d45eab52909455f4d472f1fa3e1b77d7
LiakosV <billiak@hotmail.com>
2025-07-20 21:25:45 +0300

    feat: getAllOrder and deactivateOrder added

--- frontend/src/app/shared/services/order.service.ts
+++ frontend/src/app/shared/services/order.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Order } from '../interfaces/order';
+import { Page } from '../interfaces/page';
 
 const API_URL = `${environment.apiURL}/api/orders`
 
@@ -14,5 +17,18 @@ export class OrderService {
 
   createOrder(items: { productUuid: string, quantity: number}[]) {
     return this.http.post(`${API_URL}`, {items});
+  }
+
+  getAllOrder(
+    page: number,
+    size: number,
+    sort: string,
+    direction: string
+  ): Observable<Page<Order>> {
+    return this.http.get<Page<Order>>(`${API_URL}?page=${page}&size=${size}&sortBy=${sort}&sortDirection=${direction}`);
+  }
+
+  deactivateOrder(orderUuid: string): Observable<Order> {
+    return this.http.delete<Order>(`${API_URL}/${orderUuid}`)
   }
 }
