8ddce9a78277f0dd2f18099042a52b91016c2f94
LiakosV <billiak@hotmail.com>
2025-07-20 21:25:09 +0300

    feat: create and remove added

--- frontend/src/app/shared/services/category.service.ts
+++ frontend/src/app/shared/services/category.service.ts
@@ -2,7 +2,7 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Category } from '../interfaces/category';
+import { Category, CategoryInsertDto } from '../interfaces/category';
 import { Product } from '../interfaces/product';
 import { ProductService } from './product.service';
 
@@ -19,5 +19,13 @@ export class CategoryService {
 
   getAllCategories(): Observable<Category[]> {
     return this.http.get<Category[]>(API_URL);
+  }
+
+  createCategory(category: CategoryInsertDto): Observable<Category> {
+    return this.http.post<Category>(`${API_URL}`, category);
+  }
+
+  removeCategory(id: number): Observable<Category> {
+    return this.http.delete<Category>(`${API_URL}/${id}`);
   }
 }
