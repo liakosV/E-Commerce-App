412dbbcf2e17ae66264a59856acd7412b1975ff0
LiakosV <billiak@hotmail.com>
2025-07-22 22:06:29 +0300

    fix: the productService is now been injected and the products chaged to productPage. And some getters added

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -13,6 +13,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSort, Sort } from '@angular/material/sort';
 import { RouterModule } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Page } from '../../shared/interfaces/page';
 
 
 @Component({
@@ -32,86 +33,96 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class HomeComponent {
   @ViewChild(MatSort) sort!: MatSort;
+
+  productService = inject(ProductService);
   authService = inject(AuthService);
-  cartService = inject(CartService)
+  cartService = inject(CartService);
   snackbar = inject(MatSnackBar);
 
-  displayedColumns: string[] = ['name', 'price', 'description', 'quantity', 'isActive'];
+  displayedColumns: string[] = ['name', 'price', 'description', 'quantity', 'isActive', 'actions'];
   popupOpen: { [productUuid: string]: boolean } = {};
   selectedQuantities: { [productUuid: string]: number } = {};
-  products: Product[] = [];
-  totalPages = 0;
-  page = 0;
-  size = 10;
 
+  productPage!: Page<Product>;
   sortBy = 'name';
   sortDirection = 'asc';
 
-  constructor(private productService: ProductService) {
-    this.loadProducts();
-  }
-
   ngOnInit(): void {
-    this.displayedColumns.push('actions');
+    this.loadProducts();
   }
 
   get isAdminOrSeller(): boolean {
     const role = this.authService.getUserRole();
-    return role === 'ADMIN' || role === 'SELLER'
+    return role === 'ADMIN' || role === 'SELLER';
   }
 
-togglePopup(productUuid: string): void {
-  this.popupOpen[productUuid] = !this.popupOpen[productUuid];
-  this.selectedQuantities[productUuid] = 1;
-}
-
-addToCart(product: Product): void {
-  const selectedQuantity = this.selectedQuantities[product.uuid];
-
-  if(!selectedQuantity || selectedQuantity < 1 || selectedQuantity % 1 !== 0) {
-    this.snackbar.open('Invalid quantity selected', 'Close', {duration: 3000});
-    return;
+  get products(): Product[] {
+    return this.productPage?.content || [];
   }
 
-  if (product.quantity === 0) {
-    this.snackbar.open('Product is out of stock', 'Close', { duration: 3000 });
-    return;
+  get totalPages(): number {
+    return this.productPage?.totalPages || 0;
   }
 
-  if (selectedQuantity > product.quantity) {
-    this.snackbar.open('Quantity exceeds available stock', 'Close', { duration: 3000 });
-    return;
+  get page(): number {
+    return this.productPage?.number || 0;
   }
 
-  this.cartService.addItem({
-    productUuid: product.uuid,
-    name: product.name,
-    price: product.price,
-    quantity: selectedQuantity,
-  });
+  get size(): number {
+    return this.productPage?.size || 10;
+  }
 
-  this.snackbar.open('Added to cart', 'Close', { duration: 2000 });
+  togglePopup(productUuid: string): void {
+    this.popupOpen[productUuid] = !this.popupOpen[productUuid];
+    this.selectedQuantities[productUuid] = 1;
+  }
 
-  this.popupOpen[product.uuid] = false;
+  addToCart(product: Product): void {
+    const selectedQuantity = this.selectedQuantities[product.uuid];
+
+    if (!selectedQuantity || selectedQuantity < 1 || selectedQuantity % 1 !== 0) {
+      this.snackbar.open('Invalid quantity selected', 'Close', { duration: 3000 });
+      return;
+    }
+
+    if (product.quantity === 0) {
+      this.snackbar.open('Product is out of stock', 'Close', { duration: 3000 });
+      return;
+    }
+
+    if (selectedQuantity > product.quantity) {
+      this.snackbar.open('Quantity exceeds available stock', 'Close', { duration: 3000 });
+      return;
+    }
+
+    this.cartService.addItem({
+      productUuid: product.uuid,
+      name: product.name,
+      price: product.price,
+      quantity: selectedQuantity,
+    });
+
+    this.snackbar.open('Added to cart', 'Close', { duration: 2000 });
+    this.popupOpen[product.uuid] = false;
   }
 
   loadProducts(): void {
     this.productService
       .getProducts(this.page, this.size, this.sortBy, this.sortDirection)
       .subscribe((res) => {
-        this.products = res.content;
-        this.totalPages = res.totalPages;
+        this.productPage = res;
       });
   }
 
   changePage(newPage: number) {
     if (newPage >= 0 && newPage < this.totalPages) {
-      this.page = newPage;
+      this.productPage.number = newPage;
       this.loadProducts();
     }
   }
+
   onSortChange(sort: Sort) {
-    if(!sort.direction) return;
+    if (!sort.direction) return;
     this.sortBy = sort.active;
     this.sortDirection = sort.direction || 'asc';
     this.loadProducts();
