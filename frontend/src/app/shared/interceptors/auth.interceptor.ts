6e7ee117451204c8d80b524fc0303450f03f911b
LiakosV <billiak@hotmail.com>
2025-07-22 22:11:36 +0300

    fix: the interceptor now checks if the token is expired

--- frontend/src/app/shared/interceptors/auth.interceptor.ts
+++ frontend/src/app/shared/interceptors/auth.interceptor.ts
@@ -1,7 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { EMPTY } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const token = localStorage.getItem('token');
+
+  if (authService.isTokenExpired()) {
+    authService.logout();
+  }
 
   if (token) {
     const authReq = req.clone({
