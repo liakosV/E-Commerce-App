1ec41c401315dd341993aa32bc415a45fd23308c
LiakosV <billiak@hotmail.com>
2025-07-26 20:44:36 +0300

    fix: routerLinks have been imported and the year have been changed to currentYear method

--- frontend/src/app/components/footer/footer.component.ts
+++ frontend/src/app/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+
+  currentYear: number = new Date().getFullYear();
 
 }
