60a0423b6684bcbc3dfa0ef231564473eb4bb5ad
LiakosV <billiak@hotmail.com>
2025-07-26 20:54:55 +0300

    fix: getProducts removed feat: getProductsFiltered created

--- frontend/src/app/shared/services/product.service.ts
+++ frontend/src/app/shared/services/product.service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { map, Observable } from 'rxjs';
 import { Product, ProductInsertDto } from '../interfaces/product';
 import { Page } from '../interfaces/page';
 import { environment } from '../../../environments/environment.development';
+import { ProductFilters } from '../interfaces/product-filters';
 
 
 const API_URL = `${environment.apiURL}/api/products`;
@@ -16,26 +17,33 @@ export class ProductService {
 
   constructor() { }
 
-  getProducts(
-    page: number,
-    size: number,
-    sort: string,
-    direction: string
-  ): Observable<Page<Product>> {
-    const url = `${API_URL}?page=${page}&size=${size}&sortBy=${sort}&sortDirection=${direction}`;
-    return this.http.get<Page<Product>>(url);
-  }
-
-  getAllProductsUnpaged() :Observable<Product[]> {
-    return this.http.get<Product[]>(`${API_URL}/all`)
-  }
-
   createProduct(product: ProductInsertDto): Observable<Product> {
     return this.http.post<Product>(`${API_URL}`, product)
   }
 
   removeProduct(uuid: string): Observable<Product> {
     return this.http.delete<Product>(`${API_URL}/${uuid}`);
+  }
+
+  getProductsFiltered(
+    filters: ProductFilters,
+    page: number,
+    size: number,
+    sort: string,
+    direction: string
+  ) : Observable<Page<Product>> {
+    let params = new HttpParams()
+    .set('page', page.toString())
+    .set('size', size.toString())
+    .set('sort', `${sort},${direction}`);
+
+    if (filters.category) params = params.set('category', filters.category.name);
+    if (filters.minPrice !== undefined) params = params.set('minPrice', filters.minPrice.toString());
+    if (filters.maxPrice !== undefined) params = params.set('maxPrice', filters.maxPrice.toString());
+    if (filters.isActive !== undefined) params = params.set('isActive', filters.isActive.toString());
+    if (filters.search) params = params.set('search', filters.search);
+
+    return this.http.get<Page<Product>>(`${API_URL}`, { params });
   }
   
 }
