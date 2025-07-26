54cb0e8abcef7303374515281f07029673e15fbd
LiakosV <billiak@hotmail.com>
2025-07-26 20:42:54 +0300

    fix: Categories component path have been deleted,, feat: terms,about and contact components paths have been created

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
-import { CategoriesComponent } from './components/categories/categories.component';
 import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
 import { AddProductComponent } from './components/add-product/add-product.component';
 
@@ -15,13 +14,18 @@ import { RoleSettingsComponent } from './components/admin-settings/role-settings
 import { CategorySettingsComponent } from './components/admin-settings/category-settings/category-settings.component';
 import { UserSettingsComponent } from './components/admin-settings/user-settings/user-settings.component';
 import { OrderSettingsComponent } from './components/admin-settings/order-settings/order-settings.component';
+import { TermsComponent } from './components/terms/terms.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'categories', component: CategoriesComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'terms', component: TermsComponent},
   {path: 'checkout', component: CheckoutComponent, canActivate: [authGuard]},
   {path: 'account', component: PersonalInfoComponent, canActivate: [authGuard]},
   {path: 'account/:uuid', component: PersonalInfoComponent, canActivate: [roleGuard], data: {roles: ['ADMIN']}},
@@ -36,14 +40,6 @@ export const routes: Routes = [
       {path: 'category-settings', component: CategorySettingsComponent, canActivate: [roleGuard], data: {roles: ['ADMIN']}},
       {path: 'user-settings', component: UserSettingsComponent, canActivate: [roleGuard], data: {roles: ['ADMIN']}},
       {path: 'order-settings', component: OrderSettingsComponent, canActivate: [roleGuard], data: {roles: ['ADMIN']}}
-      
     ]
-  },
-  {
-  path: 'categories/:id/products',
-  loadComponent: () =>
-    import('./components/category-products/category-products.component').then(
-      (m) => m.CategoryProductsComponent
-    ),
-}
+  }
 ];
