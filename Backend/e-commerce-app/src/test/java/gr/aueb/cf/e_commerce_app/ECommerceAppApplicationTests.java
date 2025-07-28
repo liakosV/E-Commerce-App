0205b9fcec83bc251202e1d538350b48ca9463c1
LiakosV <billiak@hotmail.com>
2025-07-28 19:13:15 +0300

    fix: ActiveProfile annotation added for the Unit Tests

--- Backend/e-commerce-app/src/test/java/gr/aueb/cf/e_commerce_app/ECommerceAppApplicationTests.java
+++ Backend/e-commerce-app/src/test/java/gr/aueb/cf/e_commerce_app/ECommerceAppApplicationTests.java
@@ -1,10 +1,14 @@
 package gr.aueb.cf.e_commerce_app;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("inmemory")
 class ECommerceAppApplicationTests {
+
 
 	@Test
 	void contextLoads() {
